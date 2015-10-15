af0792ba651ed2ee952dd89f0aff450c282210ce
BlacksheepNZ <tokenmatty@hotmail.co.nz>
2015-10-16 01:06:34 +1300

    1.0

---
+++ Assets/Resources/Champion.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Champion {
+
+    [XmlElement("ID")]
+    public float ID;
+
+    [XmlAttribute("name")]
+    public string name;
+
+    [XmlElement("picture")]
+    public float picture;
+
+    [XmlElement("attack")]
+    public float attack;
+
+    [XmlElement("health")]
+    public float health;
+
+    [XmlElement("delay")]
+    public float delay;
+
+    [XmlElement("rarity")]
+    public float rarity;
+
+    [XmlElement("type")]
+    public float type;
+
+    [XmlElement("set")]
+    public float set;
+
+    [XmlElement("skill")]
+    public float skill;
+
+    [XmlElement("skill2")]
+    public float skill2;
+}
